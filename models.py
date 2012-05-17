6b485b0a34eed8779ab6c350ec79aed5ad25184f
Joji Antony <simula67@hades.(none)>
2012-05-17 19:03:16 +0530

    Models improved

--- models.py
+++ models.py
@@ -4,25 +4,24 @@ from django.db import models
 #Currently only admin
 class User(models.Model):
     name = models.CharField(max_length=50)
-
+    password = models.CharField(max_length=50)
 # Site Data
+class Competition(models.Model):
+    name = models.CharField(max_length=50)
+class Team(models.Model):
+    name = models.CharField(max_length=50)
+    competitions = models.ManyToManyField(Competition)
 class Player(models.Model):
     name = models.CharField(max_length=50)
     jerseyNo = models.IntegerField(max_length=30)
-class Team(models.Model):
-    name = models.CharField(max_length=50)
-    players = []
-class Competition(models.Model):
-    name = models.CharField(max_length=50)
-    teams = []
+    team = models.ForeignKey(Team)
 class Goal(models.Model):
     scorer = models.ForeignKey(Player)
-    time = models.CharField(max_length=30)
+    scoreTime = models.CharField(max_length=30)
 class Fixture:
     homeTeam = models.ForeignKey(Team)
     awayTeam = models.ForeignKey(Team)
     homeTeamGoals = []
     awayTeamGoals = []
     date = models.DateField
-    league = models.ForeignKey(Competition)
-
+    competition = models.ForeignKey(Competition)
